b44ea571bac0bb098bb29bfa7b156afe5a29954f
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2017-06-10 17:04:52 +0530

    adding icons

--- resources/views/components/home-top.blade.php
+++ resources/views/components/home-top.blade.php
@@ -2,7 +2,7 @@
 <div class="columns container">
 <div class="column is-one-quarter">
 <div class="nav-left nav-menu">
-	<a class="nav-item is-tab" href="/">SPORTAL</a>
+	<a class="nav-item is-tab" href="/"><span class="icon"><i class="fa fa-home"></i></span>&nbsp; SPORTAL</a>
         @if (Auth::check())
 <!--	<a class="nav-item is-tab" href="#">Create Tournament</a>
 	<a class="nav-item is-tab" href="#">Create Profile</a> -->
@@ -10,7 +10,7 @@
 </div>
 </div>
 <div class="column is-half">
-@if(Route::currentRouteName() != 'main')
+@if(Route::currentRouteName() != ('main' || 'login' || 'register') )
 	<form method="POST" action="/search">
 	{{ csrf_field() }}
 		<p class="control has-icons-right"><input class="input is-outlined" style="margin-top:7px;" type="text" id="search" name="search" placeholder="Search Tournaments , Players , Teams ..." required>
@@ -22,12 +22,12 @@
 <div class="column is-one-quarter">
 <div class="nav-right nav-menu">
         @if (Auth::guest())
-	<a class="nav-item is-tab" href="{{ route('register') }}">Register</a>
-	<a class="nav-item is-tab" href="{{ route('login') }}">LogIn</a>
+	<a class="nav-item is-tab" href="{{ route('register') }}"><span class="icon"><i class="fa fa-user-plus"></i></span>&nbsp; Register</a>
+	<a class="nav-item is-tab" href="{{ route('login') }}"><span class="icon"><i class="fa fa-sign-in"></i></span>&nbsp; LogIn</a>
 	@else
-	<a class="nav-item is-tab" href="{{ route('register') }}">{{ Auth::user()->name }}</a>
+	<a class="nav-item is-tab" href="{{ route('register') }}"><span class="icon"><i class="fa fa-user-circle"></i></span>&nbsp; {{ Auth::user()->name }}</a>
 	<a class="nav-item is-tab" href="{{ route('logout') }}" 
-		onclick="event.preventDefault();document.getElementById('logout-form').submit();">LogOut</a>
+		onclick="event.preventDefault();document.getElementById('logout-form').submit();"><span class="icon"><i class="fa fa-sign-out"></i></span>&nbsp; LogOut</a>
 
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                  {{ csrf_field() }}
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,7 +3,7 @@
     General
   </p>
   <ul class="menu-list">
-    <li><a href="/dashboard">Dashboard</a></li>
+    <li><a href="/dashboard"><span class="icon"><i class="fa fa-dashboard"></i></span>&nbsp; Dashboard</a></li>
     <li><a>Tournament Planner</a></li>
   </ul>
   <p class="menu-label">
@@ -27,9 +27,9 @@
     Knowledge Center
   </p>
   <ul class="menu-list">
-    <li><a href="/faqs">FAQs</a></li>
-    <li><a href="/terminology">Terminology</a></li>
-    <li><a href="/erd">Database Design</a></li>
-    <li><a href="/journey">Journey</a></li>
+    <li><a href="/faqs"><span class="icon"><i class="fa fa-question"></i></span>&nbsp; FAQs</a></li>
+    <li><a href="/terminology"><span class="icon"><i class="fa fa-bullhorn"></i></span>&nbsp; Terminology</a></li>
+    <li><a href="/erd"><span class="icon"><i class="fa fa-database"></i></span>&nbsp; Database Design</a></li>
+    <li><a href="/journey"><span class="icon"><i class="fa fa-hourglass-half"></i></span>&nbsp; Journey</a></li>
   </ul>
 </aside>
